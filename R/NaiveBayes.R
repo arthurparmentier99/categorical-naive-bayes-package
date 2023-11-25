07793e0cef65d65b865c62ae165e5126b1bae5bd
arthurparmentier99 <146182092+arthurparmentier99@users.noreply.github.com>
2023-11-25 18:11:16 +0100

    last

--- R/NaiveBayes.R
+++ R/NaiveBayes.R
@@ -1,7 +1,7 @@
 install.packages("R6", repos="https://cloud.r-project.org/")
 library(R6)
-install.packages("parallel", repos="https://cloud.r-project.org/")
 library(parallel)
+library(stringr)
 
 #' Categorical Naive Bayes Classifier
 #'
